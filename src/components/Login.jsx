a7f9570671657434eaee1d949676391dfa497d28
sahilsapadiya <sahilsapadiya21@gmail.com>
2025-10-18 11:36:00 +0530

    ss

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -16,10 +16,10 @@ const Login = () => {
     }
   };
   return (
-    <div className="flex justify-center items-center min-h-screen[100vh] bg-black px-4">
+    <div className="flex justify-center items-center min-h-screen bg-black px-4">
       <div className="bg-white shadow-2xl w-full max-w-sm sm:max-w-md md:max-w-lg p-10 sm:p-16 rounded-2xl text-center">
         <h1 className="text-3xl sm:text-4xl font-bold text-[#1e293b] mb-6">
-          Welcome Back
+          WelcomeBack
         </h1>
         <p className="mb-8 text-gray-600 text-sm sm:text-base">
           Please login with your Gmail Account
